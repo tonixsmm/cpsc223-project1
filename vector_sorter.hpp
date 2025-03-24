5fe56c5855ca5ee321e67d7d615e282fe635c32e
Tony Nguyen <tonixsmm>
2025-03-24 15:17:11 -0700

    add tony's fix

--- vector_sorter.hpp
+++ vector_sorter.hpp
@@ -14,16 +14,6 @@ private:
     static void merge(std::vector<int>& vec, int left, int mid, int right);
     static void quick_sort_helper(std::vector<int>& vec, int low, int high);
     static int partition(std::vector<int>& vec, int low, int high);
-public:
-    static void merge_sort(std::vector<int>& vec);
-    static void quick_sort(std::vector<int>& vec);
-    static void insertion_sort(std::vector<int>& vec);
-
-private:
-    static void merge_sort_helper(std::vector<int>& vec, int left, int right);
-    static void merge(std::vector<int>& vec, int left, int mid, int right);
-    static void quick_sort_helper(std::vector<int>& vec, int low, int high);
-    static int partition(std::vector<int>& vec, int low, int high);
 };
 
 #endif
